9d90429976487f83134374cea49f6efb879a3b74
yingzhuo <yingzhor@gmail.com>
2019-12-20 11:16:50 +0800

    2019-12-20 11:16:50

--- java/8-springboot-onbuild/docker-entrypoint.sh
+++ java/8-springboot-onbuild/docker-entrypoint.sh
@@ -2,20 +2,30 @@
 
 set -e
 
-if [[ "${DEBUG_MODE}" == "true" ]]; then
-    flag_debug_mode="--debug"
-else
-    flag_debug_mode=""
-fi
-
+# ----------------------------------------------------------------------------------
+# check spring-boot active profiles
+# ----------------------------------------------------------------------------------
 if [[ "${SPRING_PROFILES_ACTIVE}" == "" ]]; then
     echo "[WARNING] Environment 'SPRING_PROFILES_ACTIVE' is NOT set."
 fi
 
+# ----------------------------------------------------------------------------------
+# check timezone
+# ----------------------------------------------------------------------------------
+if [[ "${TIMEZONE}" == "" ]]; then
+    echo "[WARNING] Environment 'TIMEZONE' is NOT set. Default to UTC."
+    TIMEZONE=UTC
+fi
+
+# set alias for 'TIMEZONE'
+TZ=${TIMEZONE}
+
+# ----------------------------------------------------------------------------------
+# startup
+# ----------------------------------------------------------------------------------
 exec /bin/java \
     -Djava.security.egd=file:/dev/./urandom \
-    -Duser.timezone=${TZ} \
+    -Duser.timezone=${TIMEZONE} \
     -Djava.io.tmpdir=/var/tmp \
     -jar /opt/app.jar \
-    ${flag_debug_mode} \
     "$@"
